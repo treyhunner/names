e56e8f12a6078b52e30229138f7e0d24802863b4
Trey Hunner <trey@treyhunner.com>
2013-02-14 16:09:13 -0800

    Add simple script to generate random name

---
+++ names.py
@@ -0,0 +1,39 @@
+from os.path import abspath, join, dirname
+import random
+
+
+full_path = lambda filename: abspath(join(dirname(__file__), filename))
+
+
+FILES = {
+    'first:male': full_path('dist.male.first'),
+    'first:female': full_path('dist.female.first'),
+    'last': full_path('dist.all.last'),
+}
+
+
+def get_name(filename):
+    selected = random.random() * 90
+    with open(filename) as name_file:
+        for line in name_file:
+            name, _, cummulative, _ = line.split()
+            if float(cummulative) > selected:
+                return name
+
+
+def get_first_name(gender=None):
+    if gender not in ('male', 'female'):
+        gender = random.choice(('male', 'female'))
+    return get_name(FILES['first:%s' % gender]).capitalize()
+
+
+def get_last_name():
+    return get_name(FILES['last']).capitalize()
+
+
+def get_full_name(gender=None):
+    return u"%s %s" % (get_first_name(gender), get_last_name())
+
+
+if __name__ == "__main__":
+    print get_full_name()
