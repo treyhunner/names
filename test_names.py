2ec96233fd36903680a413c0dea07f5282f7720c
Trey Hunner <trey@treyhunner.com>
2013-03-18 17:10:39 -0700

    Add test for gender file random selection

--- test_names.py
+++ test_names.py
@@ -12,11 +12,24 @@ FILES = {
 }
 
 
+class patch_file:
+
+    def __init__(self, file_dict):
+        self.files = file_dict
+
+    def __enter__(self):
+        self.old_files = names.FILES
+        names.FILES = self.files
+
+    def __exit__(self, type, value, traceback):
+        names.FILES = self.old_files
+
+
 class NamesTest(TestCase):
 
     def test_get_name(self):
         counts = defaultdict(int)
-        rounds = 1000.0
+        rounds = 5000.0
         for i in range(1, int(rounds)):
             counts[names.get_name(FILES['test1'])] += 1
         self.assertAlmostEqual(counts['Test1'] / rounds, 0.333, delta=0.05)
@@ -24,6 +37,17 @@ class NamesTest(TestCase):
         self.assertAlmostEqual(counts['Test3'] / rounds, 0.222, delta=0.05)
         self.assertAlmostEqual(counts['Test4'] / rounds, 0.166, delta=0.05)
         self.assertEqual(counts['Test5'] / rounds, 0)
+
+    def test_random_gender(self):
+        counts = defaultdict(int)
+        rounds = 5000.0
+        with patch_file({'first:male': 'test/male.txt',
+                         'first:female': 'test/female.txt'}):
+            for i in range(int(rounds)):
+                names.get_first_name()
+                counts[names.get_first_name()] += 1
+        self.assertAlmostEquals(counts['Male'] / rounds, 0.500, delta=0.05)
+        self.assertAlmostEquals(counts['Female'] / rounds, 0.500, delta=0.05)
 
 
 if __name__ == '__main__':
