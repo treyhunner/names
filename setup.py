a50da5e01d775838d3ce570fd8508188e095bed6
Trey Hunner <trey@treyhunner.com>
2013-05-14 07:26:08 -0700

    Note PyPy support

--- setup.py
+++ setup.py
@@ -31,6 +31,7 @@ setup(
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.2',
         'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: Implementation :: PyPy',
     ],
     test_suite='test_names',
 )
