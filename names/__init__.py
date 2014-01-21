11d6d707c9875107b70c45f83392e36e4a1d494d
Wallacoloo <wallacoloo@gmail.com>
2014-01-20 23:22:26 -0800

    Add functions to determine a gender given a name
    examples: is_gender("john", "male") == True. get_gender("Lucy") == "female". get_gender("Spiderman", default="superhero") == "superhero"

--- names/__init__.py
+++ names/__init__.py
@@ -41,3 +41,16 @@ def get_last_name():
 
 def get_full_name(gender=None):
     return "{0} {1}".format(get_first_name(gender), get_last_name())
+
+def is_gender(name, gender):
+    first = name.split(" ")[0].upper()
+    with open(FILES['first:%s' % gender]) as name_file:
+        for line in name_file:
+            name, _, cummulative, _ = line.split()
+            if name == first: return True
+    return False
+
+def get_gender(name, male="male", female="female", default=""):
+    if is_gender(name, "male"): return male
+    elif is_gender(name, "female"): return female
+    else: return default
