b1da59c7c45b7dc5a316ad4eb86df2fbd44e3fac
Aj <btchaggler@gmail.com>
2014-11-06 07:53:08 +0000

    Add generate random emails

--- names/__init__.py
+++ names/__init__.py
@@ -16,11 +16,31 @@ FILES = {
     'first:male': full_path('dist.male.first'),
     'first:female': full_path('dist.female.first'),
     'last': full_path('dist.all.last'),
+    'tlds': full_path('dist.tlds'),
+    'companies': full_path('dist.companies'),
 }
 
+def get_tld(filename):
+    selected = random.random() * 83
+    with open(filename) as tld_file:
+        for line in tld_file:
+            tld, cummulative = line.split()
+            if float(cummulative) > selected:
+                return tld
+    return ""
+
+def get_company(filename):
+    selected = random.random() * 60
+    with open(filename) as company_file:
+        for line in company_file:
+            cc = line.split()
+            cummulative = cc[-1]
+            if float(cummulative) > selected:
+                return ' '.join(cc[:-1])
+    return ""
 
 def get_name(filename):
-    selected = random.random() * 90
+    selected = random.random() * 91
     with open(filename) as name_file:
         for line in name_file:
             name, _, cummulative, _ = line.split()
@@ -43,3 +63,12 @@ def get_last_name():
 
 def get_full_name(gender=None):
     return "{0} {1}".format(get_first_name(gender), get_last_name())
+
+def get_email(name, company=None, tld=None):
+    if company is None:
+        company = get_company(FILES['companies'])
+    if tld is None:
+        tld = get_tld(FILES['tlds'])
+    email = '{0}@{1}.{2}'.format(name, company, tld)
+    return email.replace(' ', '').lower()
+
