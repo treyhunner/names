7281b24e12b6a7e7a29a7b8febb9fd886334ee4d
Chris Gemignani <chris.gemignani@juiceanalytics.com>
2014-11-26 15:28:57 -0600

    Allow name data to be cached to speed up generation
    - Speeds up name generation by about 500x.
    - Builds a list then uses bisect to find the selected
    random value. 
    - Assumes name data is sorted.
    
    >>> import timeit
    >>> timeit.timeit('import names; names.get_full_name()', number=1000)
    8.02225399017334
    
    >>> timeit.timeit('import names; names.get_full_name(cached=True)', number=1000)
    0.1406540870666504

--- names/__init__.py
+++ names/__init__.py
@@ -1,6 +1,7 @@
 from __future__ import unicode_literals
 from os.path import abspath, join, dirname
 import random
+from bisect import bisect_left
 
 
 __title__ = 'names'
@@ -8,9 +9,7 @@ __version__ = '0.3.0.post1'
 __author__ = 'Trey Hunner'
 __license__ = 'MIT'
 
-
 full_path = lambda filename: abspath(join(dirname(__file__), filename))
-
 
 FILES = {
     'first:male': full_path('dist.male.first'),
@@ -18,28 +17,51 @@ FILES = {
     'last': full_path('dist.all.last'),
 }
 
+# An array of (cumulative, name) pairs to use
+CACHED_FILES = {}
 
-def get_name(filename):
+
+def get_name(filename, cached=False):
+    # Choose a random name
     selected = random.random() * 90
-    with open(filename) as name_file:
-        for line in name_file:
-            name, _, cummulative, _ = line.split()
-            if float(cummulative) > selected:
-                return name
-    return ""  # Return empty string if file is empty
+    if cached:
+        # Build a cache for this filename
+        if filename not in CACHED_FILES:
+            names = []
+            with open(filename) as name_file:
+                for line in name_file:
+                    name, _, cumulative, _ = line.split()
+                    names.append((float(cumulative), name))
+            CACHED_FILES[filename] = names
+
+        names = CACHED_FILES[filename]
+        if len(names) == 0:
+            return ""  # Return empty string if file is empty
+
+        idx = min(bisect_left(names, (selected,)), len(names) - 1)
+        return names[idx][1]
+
+    else:
+        with open(filename) as name_file:
+            for line in name_file:
+                name, _, cummulative, _ = line.split()
+                if float(cummulative) > selected:
+                    return name
+        return ""  # Return empty string if file is empty
 
 
-def get_first_name(gender=None):
+def get_first_name(gender=None, cached=False):
     if gender is None:
         gender = random.choice(('male', 'female'))
     if gender not in ('male', 'female'):
         raise ValueError("Only 'male' and 'female' are supported as gender")
-    return get_name(FILES['first:%s' % gender]).capitalize()
+    return get_name(FILES['first:%s' % gender], cached).capitalize()
 
 
-def get_last_name():
-    return get_name(FILES['last']).capitalize()
+def get_last_name(cached=False):
+    return get_name(FILES['last'], cached).capitalize()
 
 
-def get_full_name(gender=None):
-    return "{0} {1}".format(get_first_name(gender), get_last_name())
+def get_full_name(gender=None, cached=False):
+    return "{0} {1}".format(get_first_name(gender, cached),
+                            get_last_name(cached))
--- test_names.py
+++ test_names.py
@@ -2,6 +2,7 @@
 import unittest
 import sys
 from os.path import abspath, join, dirname
+
 try:
     from StringIO import StringIO
 except:
@@ -29,16 +30,18 @@ class patch_stdout(object):
 
 
 class patch_file:
-
     def __init__(self, file_dict):
         self.files = file_dict
 
     def __enter__(self):
         self.old_files = names.FILES
         names.FILES = self.files
+        names.CACHED_FILES = {}
 
     def __exit__(self, type, value, traceback):
         names.FILES = self.old_files
+        names.CACHED_FILES = {}
+
 
 test_files = {
     'first:male': full_path('test/male.txt'),
@@ -48,13 +51,24 @@ test_files = {
 
 
 class NamesTest(unittest.TestCase):
-
     def test_get_name(self):
         counts = defaultdict(int)
         rounds = 5000.0
         test_file = full_path('test/file1.txt')
         for i in range(1, int(rounds)):
             counts[names.get_name(test_file)] += 1
+        self.assertAlmostEqual(counts['Test1'] / rounds, 0.333, delta=0.05)
+        self.assertAlmostEqual(counts['Test2'] / rounds, 0.277, delta=0.05)
+        self.assertAlmostEqual(counts['Test3'] / rounds, 0.222, delta=0.05)
+        self.assertAlmostEqual(counts['Test4'] / rounds, 0.166, delta=0.05)
+        self.assertEqual(counts['Test5'] / rounds, 0)
+
+    def test_get_name_cached(self):
+        counts = defaultdict(int)
+        rounds = 5000.0
+        test_file = full_path('test/file1.txt')
+        for i in range(1, int(rounds)):
+            counts[names.get_name(test_file, True)] += 1
         self.assertAlmostEqual(counts['Test1'] / rounds, 0.333, delta=0.05)
         self.assertAlmostEqual(counts['Test2'] / rounds, 0.277, delta=0.05)
         self.assertAlmostEqual(counts['Test3'] / rounds, 0.222, delta=0.05)
@@ -71,11 +85,27 @@ class NamesTest(unittest.TestCase):
         self.assertAlmostEqual(counts['Male'] / rounds, 0.500, delta=0.05)
         self.assertAlmostEqual(counts['Female'] / rounds, 0.500, delta=0.05)
 
+    def test_random_gender_cached(self):
+        counts = defaultdict(int)
+        rounds = 5000.0
+        with patch_file(test_files):
+            for i in range(int(rounds)):
+                name = names.get_first_name(cached=True)
+                counts[name] += 1
+        self.assertAlmostEqual(counts['Male'] / rounds, 0.500, delta=0.05)
+        self.assertAlmostEqual(counts['Female'] / rounds, 0.500, delta=0.05)
+
     def test_correct_files(self):
         with patch_file(test_files):
             self.assertEqual(names.get_first_name(gender='male'), "Male")
             self.assertEqual(names.get_first_name(gender='female'), "Female")
             self.assertEqual(names.get_last_name(), "Last")
+            # Test with cached responses
+            self.assertEqual(names.get_first_name(gender='male',
+                                                  cached=True), "Male")
+            self.assertEqual(names.get_first_name(gender='female',
+                                                  cached=True), "Female")
+            self.assertEqual(names.get_last_name(True), "Last")
 
     def test_empty_file(self):
         empty_files = {
@@ -87,14 +117,19 @@ class NamesTest(unittest.TestCase):
             self.assertEqual(names.get_first_name(gender='male'), "")
             self.assertEqual(names.get_first_name(gender='female'), "")
             self.assertEqual(names.get_last_name(), "")
-    
+            # Test with cached responses
+            self.assertEqual(names.get_first_name(gender='male',
+                                                  cached=True), "")
+            self.assertEqual(names.get_first_name(gender='female',
+                                                  cached=True), "")
+            self.assertEqual(names.get_last_name(True), "")
+
     def test_only_male_and_female_gender_are_supported(self):
         with self.assertRaises(ValueError):
             names.get_first_name(gender='other')
 
 
 class CommandLineTest(unittest.TestCase):
-
     def test_cli(self):
         with patch_stdout() as stdout:
             with patch_file(test_files):
